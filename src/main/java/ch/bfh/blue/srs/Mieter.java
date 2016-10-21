d06fac75509ea3982b76b93203f61118bb84da80
Christian <Christian@WINDOWS-U33LSRR>
2016-10-21 10:18:29 +0200

    Mieter.Java,Teilnehmer.java

---
+++ src/main/java/ch/bfh/blue/srs/Mieter.java
@@ -0,0 +1,14 @@
+package ch.bfh.blue.srs;
+
+import java.util.Date;
+
+public class Mieter extends Person {
+	
+	public Mieter(String name, String addresse, Date birthday) {
+		super(name, addresse, birthday);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
---
+++ src/main/java/ch/bfh/blue/srs/Teilnehmer.java
@@ -0,0 +1,12 @@
+package ch.bfh.blue.srs;
+
+import java.util.Date;
+
+public class Teilnehmer extends Person {
+
+	public Teilnehmer(String name, String addresse, Date birthday) {
+		super(name, addresse, birthday);
+		// TODO Auto-generated constructor stub
+	}
+
+}
