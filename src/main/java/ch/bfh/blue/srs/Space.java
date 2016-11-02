ebfd334466aedab4ad8da4cbb88c504f3db80ac7
Janek <Janek@Jeks_Laptop>
2016-11-02 10:15:34 +0100

    chagned mehtod isBooked()

--- src/main/java/ch/bfh/blue/srs/Space.java
+++ src/main/java/ch/bfh/blue/srs/Space.java
@@ -2,14 +2,14 @@ package ch.bfh.blue.srs;
 
 public class Space {
 	private String name;
+	private boolean booked = false;
 	
 	public Space(String name){
 		this.name=name;
 	}
 
-	public boolean isBooked() {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean isBooked() {	
+		return this.booked;
 	}
 	
 	
