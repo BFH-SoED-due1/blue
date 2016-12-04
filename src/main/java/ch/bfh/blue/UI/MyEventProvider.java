a5b4691bd8223c43f6fb40bb3728194439eba9a2
Janek <Janek@Jeks_Laptop>
2016-12-04 15:18:31 +0100

    Erweiterung des GUI für den kalender

--- src/main/java/ch/bfh/blue/UI/MyEventProvider.java
+++ src/main/java/ch/bfh/blue/UI/MyEventProvider.java
@@ -2,7 +2,6 @@ package ch.bfh.blue.UI;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.List;
 
 import com.vaadin.ui.components.calendar.event.BasicEvent;
@@ -11,24 +10,19 @@ import com.vaadin.ui.components.calendar.event.CalendarEventProvider;
 
 public class MyEventProvider implements CalendarEventProvider {
 
+	private List<CalendarEvent> events = new ArrayList<CalendarEvent>();
+
 	@Override
 	public List<CalendarEvent> getEvents(Date startDate, Date endDate) {
-		List<CalendarEvent> events = new ArrayList<CalendarEvent>();
-		
-		GregorianCalendar cal = new GregorianCalendar();
-        cal.setTime(new Date());
 
-        Date start = cal.getTime();
-        cal.add(GregorianCalendar.HOUR, 2);
-        Date end = cal.getTime();
-        BasicEvent event = new BasicEvent();
-        event.setCaption("Jek 2h");
-        event.setDescription("My Event Description");
-        event.setStart(start);
-        event.setEnd(end);
-        events.add(event);
+		BasicEvent event = new BasicEvent();
+		event.setCaption("Jek default event 1h");
+		event.setDescription("My Event Description");
+		event.setStart(startDate);
+		event.setEnd(endDate);
+		events.add(event);
 
-        return events;
+		return events;
 	}
 
 }
